6d43e6581af98dc5532fab1d5e0da29dbf2ae436
Ara Hovagumian <133039009+MicrochipDev@users.noreply.github.com>
2023-05-12 12:06:16 +0300

    ver2

---
+++ readme.md
@@ -0,0 +1,1 @@
+Playing with CSS boxes with responsive action
---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+setInterval(function () {
+    const screenWidth = window.innerWidth;
+    document.getElementById("screenWidth").innerHTML = screenWidth;
+}, 300);
